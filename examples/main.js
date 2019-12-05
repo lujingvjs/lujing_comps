d919e9e3aecd80b2cbffade1dc2f89e809a008dc
13026118978 <13026118978@163.com>
2019-12-05 19:30:49 +0800

    完善展示布局

--- examples/main.js
+++ examples/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 Vue.config.productionTip = false
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';    // 使用 CSS
 
 
 // 按需引入导入水波纹组件
 import waterRipper from '../packages/water-ripper/index'
 Vue.use(waterRipper)
+Vue.use(iView)
 
 // 全部引入
 // import lujing_comps from '../packages/index'
--- examples/views/Home.vue
+++ examples/views/Home.vue
@@ -1,15 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <Row v-for="(item, index) in dataList">
+      <Col v-for="(item2, index2) in item" span="8">
+        <div class="showBox">
+          <component :is="item2.name" /></div
+      ></Col>
+    </Row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
-  name: 'home',
-  components: {
+  data() {
+    return {
+      dataList: [
+        [
+          {
+            name: "waterRipper"
+          },
+          {
+            name: "waterRipper"
+          },
+          {
+            name: "waterRipper"
+          }
+        ],
+        [
+          {
+            name: "waterRipper"
+          },
+          {
+            name: "waterRipper"
+          },
+          {
+            name: "waterRipper"
+          }
+        ],
+        [
+          {
+            name: "waterRipper"
+          },
+          {
+            name: "waterRipper"
+          },
+          {
+            name: "waterRipper"
+          }
+        ]
+      ]
+    };
+  }
+};
+</script>
+<style lang="less"scoped>
+.home {
+  .showBox {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 400px;
+    border: 1px solid limegreen;
   }
 }
-</script>
+</style>
